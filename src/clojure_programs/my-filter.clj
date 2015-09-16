5a941d8d5fc00803a48e6ce4472f4cd3f7001aca
Your Full Name <Your EMail Id>
2015-09-16 12:13:42 +0530

    filter function using reduce method is added to file

--- src/clojure_programs/my-filter.clj
+++ src/clojure_programs/my-filter.clj
@@ -11,14 +11,31 @@
     (if (= coll ())
       newcol
       (recur fun (rest coll) (if (= true (fun (first coll)))
-                               (conj newcol (first coll))
+                               (conj (vec newcol) (first coll))
                                newcol )))))
 
 
-;;using bad recursion method
 
-(defn my-filter [f coll]
-  (if (first coll)
-    (cons (when (= true (f (first coll))) (first coll))
-          (my-filter1 f (rest coll)))
-    (list)))
+;; Using reduce function we can create a filter function
+(defn my-filter2 [function collection]
+  (reduce (fn [result element]
+            (if (function element)
+              (conj result element)
+              result
+              ))
+          []
+          collection))
+
+
+
+
+;;using bad recursion method
+;;infact this function is bad also but i am trying to implement
+;; soory its not working
+
+( defn my-filter [f coll]
+  (def result [])
+  (if-not (= (first coll) nil)
+    (if (f (first coll))
+      (conj (my-filter f (rest coll)) (first coll)))
+    result))
