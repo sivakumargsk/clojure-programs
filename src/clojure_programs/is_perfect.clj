6632f12f6561f8c9ea7d43ee6d15a5fc847f6658
sivakumargsk <geddasiva777@gmail.com>
2015-09-21 22:00:06 +0530

    adding some commits

--- src/clojure_programs/is_perfect.clj
+++ src/clojure_programs/is_perfect.clj
@@ -5,8 +5,9 @@
 ;; i.e the all positive divisor sum is equal to that number
 ;; eg: 6 = 3 + 2 + 1 where 3 2 1 are their divisors
 
-;;for this program i create a function divisors which stores list of all possible
+;;for this program i create a function divisors which stores list of all ;;possible excluding the given number
 ;;divisors for the given element.....
+
 (defn divisors [n]
   (reduce (fn [result element]
             (if (= 0 (mod n element))
@@ -15,9 +16,23 @@
           []
           (range 1 (inc (/ n 2)))))
 
-;; now calling the above function in the is_perfect function.
+;;clojure-programs.core> (divisors 99)
+;;[1 3 9 11 33]
+;;clojure-programs.core> (divisors 101)
+;;[1]
+;;clojure-programs.core> (divisors 28)
+;;[1 2 4 7 14]
 
+
+;; now calling the above function in the is_perfect function.
 (defn is_perfect [n]
   (if (= n (reduce + (divisors n)))
     true
     false))
+
+;;clojure-programs.core> (is_perfect 6)
+;;true
+;;clojure-programs.core> (is_perfect 28)
+;;true
+;;clojure-programs.core> (is_perfect 99)
+;;false
