09f1b3fd6334b6b35b8daf83f79cfe982c8c7faf
sivakumargsk <geddasiva777@gmail.com>
2015-09-21 21:57:56 +0530

    adding some comments

--- src/clojure_programs/findingChar.clj
+++ src/clojure_programs/findingChar.clj
@@ -1,14 +1,18 @@
 (ns clojure-programs.core
   (:gen-class))
 
+;;This function returns the number of times the charecter is repeated in the given string.
 (defn find-rept-char-str [ch w]
   "This function returns the number of times the charecter is repeated in the given string."
-  (loop [character ch
-         word w
+  (loop [word w
          sum 0]
     (if (= word "")
       sum
-      (recur character (apply str (rest word))
-             (if (= character (first word))
-               (inc sum)
-               sum )))))
+      (recur  (apply str (rest word))(if (= ch (first word))
+                                       (inc sum)
+                                       sum )))))
+
+;;clojure-programs.core> (find-rept-char-str \a "siva kumar")
+;;2
+;;clojure-programs.core> (find-rept-char-str \a "potter")
+;;0
