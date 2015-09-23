8bcf64e755ef173478519b29e5c3f90c77e582f8
Your Full Name <Your EMail Id>
2015-09-23 13:21:02 +0530

    changed at function to in place of apply str we use clojure.string/join

--- src/clojure_programs/palindrome.clj
+++ src/clojure_programs/palindrome.clj
@@ -5,7 +5,7 @@
 ;; if it looks the same forwards and backwards.
 (defn is-palindrome [word]
   "This function returns a true if the given string is palindrome or false if not"
-  (= word (apply str (reverse word))))
+  (= word (clojure.string/join (reverse word))))
 
 ;;clojure-programs.core> (is-palindrome "malayalam")
 ;;true
