881319e2be183326a1ae4c5f238b117b687a3c53
sivakumargsk <geddasiva777@gmail.com>
2015-09-24 19:21:55 +0530

    removing old functions adding new function

--- src/clojure_programs/is_perfect.clj
+++ src/clojure_programs/is_perfect.clj
@@ -1,39 +1,13 @@
-(ns clojure-programs.core
-  (:gen-class))
+(ns clojure-programs.core)
 
-;;function for finding the given number is perfect or not
-;; i.e the all positive divisor sum is equal to that number
-;; eg: 6 = 3 + 2 + 1 where 3 2 1 are their divisors
-
-;;for this program i create a function divisors which stores list of all
-;;possible excluding the given number
-;;divisors for the given element.....
-
-(defn divisors [n]
-  (reduce (fn [result element]
-            (if (= 0 (mod n element))
-              (conj result element)
-              result))
-          []
+(defn divisors-sum [n]
+  (reduce (fn [x y]
+            (if (= 0 (mod n y))
+              (+ x y)
+              (+ x 0)))
           (range 1 (inc (/ n 2)))))
 
-;;clojure-programs.core> (divisors 99)
-;;[1 3 9 11 33]
-;;clojure-programs.core> (divisors 101)
-;;[1]
-;;clojure-programs.core> (divisors 28)
-;;[1 2 4 7 14]
-
-
-;; now calling the above function in the is_perfect function.
-(defn is_perfect [n]
-  (if (= n (reduce + (divisors n)))
+(defn is-perfect [n]
+  (if (= n (divisors-sum n))
     true
     false))
-
-;;clojure-programs.core> (is_perfect 6)
-;;true
-;;clojure-programs.core> (is_perfect 28)
-;;true
-;;clojure-programs.core> (is_perfect 99)
-;;false
