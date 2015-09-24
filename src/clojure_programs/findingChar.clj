7c178908265252f129fec656de585e3f23f585b1
Your Full Name <Your EMail Id>
2015-09-24 16:31:51 +0530

    removing gen-class, apply str and changing word to vector

--- src/clojure_programs/findingChar.clj
+++ src/clojure_programs/findingChar.clj
@@ -1,18 +1,12 @@
-(ns clojure-programs.core
-  (:gen-class))
+(ns clojure-programs.core)
 
 ;;This function returns the number of times the charecter is repeated in the given string.
 (defn find-rept-char-str [ch w]
   "This function returns the number of times the charecter is repeated in the given string."
-  (loop [word w
+  (loop [word (vec w)
          sum 0]
     (if (empty? word)
       sum
-      (recur  (apply str (rest word))(if (= ch (first word))
-                                       (inc sum)
-                                       sum )))))
-
-;;clojure-programs.core> (find-rept-char-str \a "siva kumar")
-;;2
-;;clojure-programs.core> (find-rept-char-str \a "potter")
-;;0
+      (recur  (rest word) (if (= ch (first word))
+                            (inc sum)
+                            sum )))))
