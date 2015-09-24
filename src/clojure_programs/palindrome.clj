a07e8c3baeb4c2f66e7f88ec52e037341d5c6c3e
Your Full Name <Your EMail Id>
2015-09-24 16:44:35 +0530

    removing join fun just apply cstr/reverse word

--- src/clojure_programs/palindrome.clj
+++ src/clojure_programs/palindrome.clj
@@ -1,13 +1,6 @@
 (ns clojure-programs.core
-  (:gen-class))
+  (:require [clojure.string :as cstr]))
 
-;; function to find out if a string is a palindrome–that is,
-;; if it looks the same forwards and backwards.
 (defn is-palindrome [word]
   "This function returns a true if the given string is palindrome or false if not"
-  (= word (clojure.string/join (reverse word))))
-
-;;clojure-programs.core> (is-palindrome "malayalam")
-;;true
-;;clojure-programs.core> (is-palindrome "srinivas")
-;;false
+  (= word (cstr/reverse word)))
