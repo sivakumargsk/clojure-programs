5a0e988606445ab4976cd70d7ad049c65562c13c
Your Full Name <Your EMail Id>
2015-09-24 16:28:46 +0530

    Removing gen-class and comments

--- src/clojure_programs/factorial.clj
+++ src/clojure_programs/factorial.clj
@@ -1,9 +1,6 @@
-(ns clojure-programs.core
-  (:gen-class))
+(ns clojure-programs.core)
 
 
-;; function for finding a factorial of the given number
-;; using tail recursion functionality
 (defn factorial [x]
   (loop [n x
          acc 1]
@@ -12,20 +9,12 @@
       (recur (dec n) (* n acc) ))))
 
 
-;; function for finding a factorial of the given number
-;; using reduce method
 (defn factorial1 [n]
   (reduce * (range 1 (inc n))))
 
 
-;; function for finding a factorial of the given number
-;; using recursion method-sig
+
 (defn factorial2 [n]
   (if (= n 0)
     1
     (* n (factorial2 (dec n)))))
-
-;;clojure-programs.core> (factorial2 6)
-;;720
-;;clojure-programs.core> (factorial2 0)
-;;1
