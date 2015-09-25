06ef6a55cd7f7162908c73f64489dab5b6bf2b80
sivakumargsk <geddasiva777@gmail.com>
2015-09-25 09:21:23 +0530

    creating a map to strings and line-no and removing the index of function.

--- src/clojure_programs/grep.clj
+++ src/clojure_programs/grep.clj
@@ -4,18 +4,17 @@
 
 ;;function for finding the given string is present in multiline string
 
-(defn grep [word multiline_string]
-  "it returns the line numbers where the given word is appered in string"
-  (def list-of-strings (cstr/split-lines multiline_string))
-  (reduce (fn [result string]
-            (if (.contains string word)
-              (conj result (inc (.indexOf list-of-strings string)))
-              result))
-          []
-          list-of-strings))
+(defn grep [word multiline-string]
+  (let [list-of-strings (cstr/split-lines multiline-string)
+        line-no-string (zipmap list-of-strings (range 1 (inc (count list-of-strings))))]
+    (reduce (fn [result string]
+              (if (.contains string word)
+                (conj result (line-no-string string))
+                result))
+            []
+            list-of-strings)))
 
 ;;user>(grep "cin" "cout << \"Enter two numbers....\";\n cin >> number1;\n cin >> number2;")
 ;; [2 3]
-
 
 
