261a28e4b2737eefde7d5d32c039cd83ea2c96e8
Your Full Name <Your EMail Id>
2015-10-16 12:11:41 +0530

    changed at if statement

--- src/clojure_programs/is_perfect.clj
+++ src/clojure_programs/is_perfect.clj
@@ -1,9 +1,9 @@
 (ns clojure-programs.core)
 
 (defn is-perfect [n]
-  (reduce (fn [result element]
-            (if (= 0 (mod result element))
-              (+ result element)
-              result))
-          0
-          (range 1 (inc (/ n 2)))))
+  (= n (reduce (fn [result element]
+                 (if (= 0 (mod n element))
+                   (+ result element)
+                   result))
+               0
+               (range 1 (inc (/ n 2))))))
